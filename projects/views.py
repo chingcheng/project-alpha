f249d08fd688b008d4e439c5a102148a2383c6d2
Ching Cheng <ching@chings-air.lan>
2022-12-06 18:33:13 -0800

    Feature 14 complete

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from projects.models import Project
 from tasks.models import Task
+from projects.forms import ProjectForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -12,6 +13,7 @@ def list_projects(request):
     context = {"projects_list": projects_list}
     return render(request, "projects/list.html", context)
 
+
 @login_required
 def show_project(request, id):
     project_details = get_object_or_404(Project, id=id)
@@ -19,3 +21,20 @@ def show_project(request, id):
         "project_details": project_details
     }
     return render(request, "projects/detail.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(False)
+            project.owner = request.user
+            project.save()
+            return redirect("list_projects")
+    else:
+        form = ProjectForm()
+    context = {
+        "form": form
+    }
+    return render(request, "projects/create.html", context)
--- tasks/models.py
+++ tasks/models.py
@@ -21,5 +21,5 @@ class Task(models.Model):
         on_delete=models.CASCADE
     )
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
